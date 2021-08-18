8edd1f580f5aebdf8508c3b43d78c97b812dabd3
Saptarshi <saptarshi.bagchi@codeclouds.in>
2021-08-18 11:17:34 +0530

    queue-group added

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,19 +1,24 @@
-import nats, {Message} from 'node-nats-streaming';
-
+import nats, { Message } from "node-nats-streaming";
 
 console.clear();
-const stan = nats.connect(
-    'ticketing', '123', {
-        url : 'http://localhost:4222'
-    }
-)
+const stan = nats.connect("ticketing", "123", {
+	url: "http://localhost:4222",
+});
 
-stan.on('connect', () => {
-    console.log('listening from NATS')
-    const options = stan.subscriptionOptions().setManualAckMode(true).setDeliverAllAvailable().setDurableName('sample-listener');
-    const subscription = stan.subscribe('ticket:created', options);
+stan.on("connect", () => {
+	console.log("listening from NATS");
+	const options = stan
+		.subscriptionOptions()
+		.setManualAckMode(true)
+		.setDeliverAllAvailable()
+		.setDurableName("sample-listener");
+	const subscription = stan.subscribe(
+		"ticket:created",
+		"queue-group-name",
+		options
+	);
 
-    subscription.on('message', (data : Message) => {
-        console.log(data.getData())
-    })
-})
+	subscription.on("message", (data: Message) => {
+		console.log(data.getData());
+	});
+});
